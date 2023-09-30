db9e8c5b3c8b86df5ab419708d09548065df1c54
Hamid <www.abdulhamid3311@gmail.com>
2023-09-30 13:01:49 +0600

    added modal for delete book

--- src/pages/BookDetails.tsx
+++ src/pages/BookDetails.tsx
@@ -1,12 +1,18 @@
 import { useNavigate, useParams } from 'react-router-dom';
 import { useDeleteBookMutation, useGetSingleBookQuery } from '@/redux/features/books/bookApi';
 import { toast } from 'react-toastify';
+import { Button, Modal } from 'flowbite-react';
+import { useState } from 'react';
+import { HiOutlineExclamationCircle } from "react-icons/hi"
 
 const BookDetails = () => {
     const { id } = useParams();
     const { data: getBook, error, isLoading } = useGetSingleBookQuery(id);
     const [deleteBook, { isLoading: isDeleteing }] = useDeleteBookMutation();
     const navigate = useNavigate();
+
+    const [openModal, setOpenModal] = useState<string | undefined>();
+    const props = { openModal, setOpenModal };
 
 
     if (isLoading) {
@@ -37,50 +43,79 @@ const BookDetails = () => {
     };
 
     return (
-        <div className='max-w-7xl mx-auto px-5 lg:px-0'>
-            <div className='max-w-4xl mx-auto bg-gray-200 p-5 m-5'>
-                <h1 className='text-2xl font-bold'>Title: {getBook?.title}</h1>
-                <div>
-                    <p>Author: {getBook?.author}</p>
-                    <p>Genre: {getBook?.genre}</p>
-                    <p>Publication Date: {getBook?.publicationDate}</p>
+        <>
 
-                    <div className="flex items-center gap-2 my-2">
-                        <button
-                            onClick={() => handleEditPage(getBook?._id)}
-                            className='bg-blue-500 hover:bg-blue-700 text-white px-3 py-1 rounded-[5px] text-base font-semibold '>Edit</button>
+            <div className='max-w-7xl mx-auto px-5 lg:px-0'>
+                <div className='max-w-4xl mx-auto bg-gray-200 p-5 m-5'>
+                    <h1 className='text-2xl font-bold'>Title: {getBook?.title}</h1>
+                    <div>
+                        <p>Author: {getBook?.author}</p>
+                        <p>Genre: {getBook?.genre}</p>
+                        <p>Publication Date: {getBook?.publicationDate}</p>
 
-                        <button
-                            onClick={() => handleDeleteBook(getBook?._id)}
-                            disabled={isDeleteing}
-                            className='bg-red-500 hover:bg-red-700 text-white px-3 py-1 rounded-[5px] text-base font-semibold '>    {isDeleteing ? "Deleting..." : "Delete"}</button>
-                    </div>
-                </div>
+                        <div className="flex items-center gap-2 my-2">
+                            <button
+                                onClick={() => handleEditPage(getBook?._id)}
+                                className='bg-blue-500 hover:bg-blue-700 text-white px-3 py-1 rounded-[5px] text-base font-semibold '>Edit</button>
 
-                <div className='mt-5'>
-
-                    <form>
-                        <h1 className='font-bold'>Leave a comment</h1>
-                        <div className="w-full" id="textarea">
-                            <textarea
-                                placeholder="Write Here..." required rows={2} className='w-full' />
+                            <button
+                                onClick={() => {
+                                    props.setOpenModal('pop-up')
+                                }}
+                                disabled={isDeleteing}
+                                className='bg-red-500 hover:bg-red-700 text-white px-3 py-1 rounded-[5px] text-base font-semibold '>    {isDeleteing ? "Deleting..." : "Delete"}</button>
                         </div>
-                        <button className='bg-primary text-white px-3 py-1 rounded-[5px]  font-semibold '>Submit</button>
+                    </div>
 
-                    </form>
+                    <div className='mt-5'>
+
+                        <form>
+                            <h1 className='font-bold'>Leave a comment</h1>
+                            <div className="w-full" id="textarea">
+                                <textarea
+                                    placeholder="Write Here..." required rows={2} className='w-full' />
+                            </div>
+                            <button className='bg-primary text-white px-3 py-1 rounded-[5px]  font-semibold '>Submit</button>
+
+                        </form>
 
 
-                    {/* Comment show section */}
-                    <div className='mt-10'>
-                        <h3 className='text-lg font-bold text-primary' >Comments:</h3>
-                        {
-                            getBook?.reviews?.map(rev => <p>
-                                {rev}</p>)
-                        }
+                        {/* Comment show section */}
+                        <div className='mt-10'>
+                            <h3 className='text-lg font-bold text-primary' >Comments:</h3>
+                            {
+                                getBook?.reviews?.map(rev => <p>
+                                    {rev}</p>)
+                            }
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+
+
+            {/* Delete Book Modal */}
+
+            <Modal show={props.openModal === 'pop-up'} size="md" popup onClose={() => props.setOpenModal(undefined)}>
+                <Modal.Header />
+                <Modal.Body>
+                    <div className="text-center">
+                        <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
+                        <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+                            Are you sure you want to delete this product?
+                        </h3>
+                        <div className="flex justify-center gap-4">
+                            <Button color="failure" onClick={() => handleDeleteBook(getBook?._id)}>
+                                Yes, I'm sure
+                            </Button>
+                            <Button color="gray" onClick={() => props.setOpenModal(undefined)}>
+                                No, cancel
+                            </Button>
+                        </div>
+                    </div>
+                </Modal.Body>
+            </Modal>
+        </>
+
     );
 };
 
