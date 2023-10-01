4e5ae266c921153546c1a1c02e2a0ee42d9c9b8f
Hamid <www.abdulhamid3311@gmail.com>
2023-10-01 23:01:38 +0600

    updated banner content

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import Footer from "@/Layouts/Footer";
 import RecentBooks from "@/components/RecentBooks";
+import { Link } from "react-router-dom";
 
 export default function Home() {
     return (
@@ -21,10 +22,10 @@ export function Banner() {
         <div className=" flex items-center justify-center min-h-screen max-w-7xl mx-auto bg-white">
 
             <div className="flex flex-col items-center h-full gap-5">
-                <h1 className="text-3xl lg:text-6xl font-bold ">Welcome To The Book Shop Store</h1>
-                <p className="text-center ">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni tenetur quae dolorem alias, aliquid a perspiciatis esse mollitia dicta sequi. Saepe modi inventore ut! Quia ut aspernatur temporibus nostrum? Quo.</p>
+                <h1 className="text-3xl lg:text-6xl font-bold ">Welcome To The Book Shop </h1>
+                <p className="text-center ">Discover a captivating array of books in our catalog, ranging from thrilling adventures to insightful knowledge. Immerse yourself in a world of literary wonder and find the perfect read for every mood and interest. Your next great adventure awaits within the pages of our diverse collection.</p>
 
-                <a href="#" className="flex items-center justify-center px-5 py-3 text-base font-medium text-center bg-primary text-white  rounded-[5px]">Let's Explore </a>
+                <Link to="/all-books" className="flex items-center justify-center px-5 py-3 text-base font-medium text-center bg-primary text-white  rounded-[5px]">Let's Explore </Link>
 
 
 
